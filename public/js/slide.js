94ead3ec6a658e77a7b68f4d59f3ee07cb78d7c5
tsionasmamaw <tayimer@yahoo.com>
2016-12-16 01:27:41 -0800

    center slide add pic

--- public/js/slide.js
+++ public/js/slide.js
@@ -1,51 +1,12 @@
-// <script>
-//   $(function() {
-//     $(".rslides").responsiveSlides();
-//   });
-// </script>
-// $(".rslides").responsiveSlides({
-// 	auto:true,
-//     speed: 563,
-//     timeout: 4000;
-// });
-
-
-// $(function() {
-// 	//configuration
-// 	var width= 100%;
-// 	var animationSpeed =1000;
-// 	var pause = 3000;
-// 	var currentSlide =1;
-
-// 	var $slider + $('#slider');
-// 	var $slideContainer= $slider.find('.slides');
-// 	var $slides = $slideContainer.find('.slide');
-
-// 	var interval;
-
-// 	function startSlider(){
-// 		interval = setInterval(function (){
-// 		$slideContainer.animate({'margin-left':'-=' +width}, animationSpeed,function(){
-// 			currentSlide++;
-// 			if(currentSlide===$slides.length){
-// 				currentSlide=1;
-// 				$slideContainer.css('margin-left',0);
-// 			}
-// 		});
-// 		}, pause); 
-// 	}
-// 	function stopSlider(){
-// 		clearInterval(interval);
-// 	}	
-// 	$slider.on('mouseenter', stopSlider).on('mouseleave',startSlider);
-// 	// startSlider();
+// $(document).ready(function() {
+//     $('#gallery').fullpage();
 // });
 
 
 $(function() {
 	//configuration
-	var height= 650;
-	var animationSpeed =1000;
+	var height= 550;
+	var animationSpeed =3000;
 	var pause = 2000;
 	var currentSlide =1;
 
